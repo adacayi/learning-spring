f5d8c3d674d3a6b35e3db9b452f1597d8d8562b5
Abdullah Sanver <a_sanver@hotmail.com>
2018-06-20 17:23:58 +0100

    scope proxymode for xml configuration added.

---
+++ learning-spring/src/main/java/scope_prototype_xml_proxymode/App.java
@@ -0,0 +1,20 @@
+package scope_prototype_xml_proxymode;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"scope_prototype_xml_proxymode/config.xml")) {
+			MediaPlayer player = context.getBean(MediaPlayer.class); // If we set Yunus as prototype and set proxyMode
+			// to TARGET_CLASS, then a proxy to Yunus is
+			// inserted in every injection and when it is
+			// called it will get a bean for Yunus.
+			player.play();
+			player = context.getBean(MediaPlayer.class);
+			player.play();
+		}
+	}
+
+}
---
+++ learning-spring/src/main/java/scope_prototype_xml_proxymode/Yunus.java
@@ -0,0 +1,25 @@
+package scope_prototype_xml_proxymode;
+
+public class Yunus implements CompactDisc {
+
+	private String title = "Daglar ile";
+	private String artist = "Yunus";
+	private int id;
+	private static int lastId = 1;
+
+	private static Object lockObject = new Object();
+
+	public Yunus() {
+		synchronized (lockObject) {
+			id = lastId;
+			lastId++;
+		}
+	}
+
+	@Override
+	public void play() {
+		System.out.printf("Playing %d- %s - %s\n", id, artist, title);
+
+	}
+
+}
