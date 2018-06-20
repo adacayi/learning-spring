12b01f22d33e5803b7860ed53831bb9a5fc4ecf2
Abdullah Sanver <a_sanver@hotmail.com>
2018-06-20 17:04:37 +0100

    proxymode sample added.

---
+++ learning-spring/src/main/java/scope_prototype_java_proxymode/App.java
@@ -0,0 +1,19 @@
+package scope_prototype_java_proxymode;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class)) {
+			MediaPlayer player = context.getBean(MediaPlayer.class); // If we set Yunus as prototype and set proxyMode
+																		// to TARGET_CLASS, then a proxy to Yunus is
+																		// inserted in every injection and when it is
+																		// called it will get a bean for Yunus.
+			player.play();
+			player = context.getBean(MediaPlayer.class);
+			player.play();
+		}
+	}
+
+}
---
+++ learning-spring/src/main/java/scope_prototype_java_proxymode/MediaPlayer.java
@@ -0,0 +1,5 @@
+package scope_prototype_java_proxymode;
+
+public interface MediaPlayer {
+	void play();
+}
