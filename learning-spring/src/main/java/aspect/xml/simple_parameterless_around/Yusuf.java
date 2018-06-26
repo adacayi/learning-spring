45575629a5c207470ff8259337dd5226a7513bde
Abdullah Sanver <a_sanver@hotmail.com>
2018-06-26 15:13:48 +0100

    aspect.xml.simple_parameterless_around added.

---
+++ learning-spring/src/main/java/aspect/xml/simple_parameterless_around/Yusuf.java
@@ -0,0 +1,13 @@
+package aspect.xml.simple_parameterless_around;
+
+public class Yusuf implements Performance {
+
+	@Override
+	public void perform() {
+		System.out.println("Daglar ile taslar ile");
+		System.out.println("Cagirayim Mevlam seni");
+		System.out.println("Seherlerde kuslar ile");
+		System.out.println("Cagirayim Mevlam seni");
+	}
+
+}
