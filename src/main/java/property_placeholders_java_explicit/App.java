556511684448e05433b7d0688951e73409c41634
Abdullah Sanver <a_sanver@hotmail.com>
2018-08-29 15:06:45 +0100

    Property placeholder samples refactored and properties moved to resources folder.

--- src/main/java/property_placeholders_java_explicit/App.java
+++ src/main/java/property_placeholders_java_explicit/App.java
@@ -11,31 +11,15 @@ import org.springframework.core.env.Environment;
 
 public class App {
 
-	public static void main(String[] args) {
-		System.setProperty("spring.profiles.active", "prod,firstMachine");
-		Properties properties = System.getProperties();
-		try {
-			properties.load(Files.newInputStream(Paths.get("src/main/java/property_placeholders_java/app.properties")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class)) {
-			CompactDisc cd = context.getBean(CompactDisc.class);
-			cd.play();
-
-			Environment env = context.getEnvironment();
-			System.out.println("\nActive Profiles:\n");
-
-			Arrays.asList(env.getActiveProfiles()).forEach(System.out::println);
-
-			System.out.println();
-
-			if (!env.acceptsProfiles("dev"))
-				System.out.println("We do not have dev profile");
-			if (env.acceptsProfiles("prod"))
-				System.out.println("We have prod profile");
-			if (env.acceptsProfiles("prod", "firstMachine"))
-				System.out.println("We have prod and firstMachine profile");
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            System.getProperties().load(App.class.getResource("app.properties").openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class)) {
+            CompactDisc cd = context.getBean(CompactDisc.class);
+            cd.play();
+        }
+    }
 }
--- src/main/java/property_placeholders_xml/App.java
+++ src/main/java/property_placeholders_xml/App.java
@@ -12,10 +12,9 @@ import org.springframework.core.env.Environment;
 public class App {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.profiles.active", "prod,firstMachine");
 		Properties properties = System.getProperties();
 		try {
-			properties.load(Files.newInputStream(Paths.get("src/main/java/property_placeholders_xml/app.properties")));
+			properties.load(App.class.getResource("app.properties").openStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -23,20 +22,6 @@ public class App {
 				"property_placeholders_xml/config.xml")) {
 			CompactDisc cd = context.getBean(CompactDisc.class);
 			cd.play();
-
-			Environment env = context.getEnvironment();
-			System.out.println("\nActive Profiles:\n");
-
-			Arrays.asList(env.getActiveProfiles()).forEach(System.out::println);
-
-			System.out.println();
-
-			if (!env.acceptsProfiles("dev"))
-				System.out.println("We do not have dev profile");
-			if (env.acceptsProfiles("prod"))
-				System.out.println("We have prod profile");
-			if (env.acceptsProfiles("prod", "firstMachine"))
-				System.out.println("We have prod and firstMachine profile");
 		}
 	}
 }
