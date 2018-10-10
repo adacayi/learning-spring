f7ff752d7b652e577912a8bff747679b576b97ea
Abdullah Sanver <a_sanver@hotmail.com>
2018-10-10 17:38:02 +0100

    In property_placeholders_java and property_placeholders_java_explicit, PropertySourcesPlaceholderConfigurer is removed because the code works without it.

--- src/main/java/property_placeholders_java_explicit/Config.java
+++ src/main/java/property_placeholders_java_explicit/Config.java
@@ -3,7 +3,6 @@ package property_placeholders_java_explicit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 public class Config {
@@ -11,10 +10,5 @@ public class Config {
     public CompactDisc compactDisc(@Value("${disc.artist}") String artist, @Value("${disc.title}") String title,
                                    @Value("${disc.trackCount}") String trackCount, @Value("${disc.size}") double size) {
         return new Yunus(artist, title, trackCount, size);
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
     }
 }
